829494560da6287ec43781f32c38dc5f10cfecfe
ac3-bugs <70935503+ac3-bugs@users.noreply.github.com>
2021-03-24 22:08:19 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,27 +6,7 @@ import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-
+window.location="https://discord.gg/zdvxQ3dr2x";
+// -->
+</script>
 export default withAuthenticator(App, true);
